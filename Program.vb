768f0af0741fa13d0176d1fc7cdd5f3d7e225153
Rebecca Skittles <111042901+RebeccaSkittles@users.noreply.github.com>
2023-06-18 18:26:55 +1000

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -1,13 +1,16 @@
 Imports System.Net
 Imports System.Net.Sockets
 Imports System.IO
+Imports System.Xml
+Imports System.Windows.Forms
 
 Module Program
     Dim databaseFilePath As String = ""
-    Dim currentBuildVersion As String = "02A23"
+    Dim currentBuildVersion As String = "03A23"
 
     Sub Main()
         Dim port As Integer = 8080
+        Dim alternativePort As Integer = 80
         Dim ipAddress As IPAddress = GetLocalIPAddress()
 
         Console.WriteLine("Universal Server")
@@ -41,12 +44,47 @@ Module Program
                 Console.ForegroundColor = ConsoleColor.White
 
                 ' Handle client communication...
+                Try
+                    Dim reader As New StreamReader(clientSocket.GetStream())
+                    Dim writer As New StreamWriter(clientSocket.GetStream())
+                    writer.AutoFlush = True
 
-                clientSocket.Close()
+                    Dim command As String = reader.ReadLine()
 
-                Console.ForegroundColor = ConsoleColor.Red
-                Console.WriteLine("[INFO]: Client at {0} has disconnected from the server. Time: {1}", clientIPAddress, DateTime.Now.ToString())
-                Console.ForegroundColor = ConsoleColor.White
+                    If command = "userver.request(code)" Then
+                        Dim generatedCode As Integer = GenerateRandomCode()
+                        writer.WriteLine(generatedCode)
+                        Console.ForegroundColor = ConsoleColor.Green
+                        Console.WriteLine("[INFO]: Client at {0} has requested a random code.", clientIPAddress)
+                        Console.ForegroundColor = ConsoleColor.White
+                        Console.WriteLine("[INFO]: Code has been sent to client at {0}", clientIPAddress)
+                    Else
+                        Console.ForegroundColor = ConsoleColor.Yellow
+                        Console.WriteLine("[WARNING]: Invalid command received from client at {0}", clientIPAddress)
+                        Console.ForegroundColor = ConsoleColor.White
+                    End If
+
+                Catch ex As IOException
+                    Console.ForegroundColor = ConsoleColor.Yellow
+                    Console.WriteLine("[WARNING]: An IO exception occurred while handling client at {0}. Reason: {1}", clientIPAddress, ex.Message)
+                    Console.ForegroundColor = ConsoleColor.Yellow
+                    Console.WriteLine("[WARNING]: Client at {0} has been kicked from the server. Reason: {1}", clientIPAddress, ex.Message)
+                    Console.ForegroundColor = ConsoleColor.White
+
+                Catch ex As Exception
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine("[ERROR]: Unknown error occurred. Reason: {0}", ex.Message)
+                    Console.ForegroundColor = ConsoleColor.White
+
+                    ShowErrorMessageBox(ex.Message)
+                Finally
+                    clientSocket.Close()
+
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine("[INFO]: Client at {0} has disconnected from the server. Time: {1}", clientIPAddress, DateTime.Now.ToString())
+                    Console.ForegroundColor = ConsoleColor.White
+                End Try
+
             End While
 
             serverSocket.Stop()
@@ -55,6 +93,8 @@ Module Program
             Console.ForegroundColor = ConsoleColor.Red
             Console.WriteLine("[ERROR]: Server failed to start on IP: {0} On Port: {1}. Reason: {2}", ipAddress, port, ex.Message)
             Console.ForegroundColor = ConsoleColor.White
+
+            ShowErrorMessageBox(ex.Message)
         End Try
 
         Console.WriteLine("Press any key to exit...")
@@ -86,9 +126,18 @@ Module Program
         ' Check if the database file exists in the same directory as the server app
         databaseFilePath = Path.Combine(Directory.GetCurrentDirectory(), "database.xml")
         If File.Exists(databaseFilePath) Then
-            Console.ForegroundColor = ConsoleColor.Green
-            Console.WriteLine("[INFO]: Database is connected to the server.")
-            Console.ForegroundColor = ConsoleColor.White
+            Try
+                Dim xmlDoc As New XmlDocument()
+                xmlDoc.Load(databaseFilePath)
+
+                Console.ForegroundColor = ConsoleColor.Green
+                Console.WriteLine("[INFO]: Database is connected to the server.")
+                Console.ForegroundColor = ConsoleColor.White
+            Catch ex As XmlException
+                Console.ForegroundColor = ConsoleColor.Yellow
+                Console.WriteLine("[WARNING]: The database file does not have the expected XML structure.")
+                Console.ForegroundColor = ConsoleColor.White
+            End Try
         Else
             Console.ForegroundColor = ConsoleColor.Yellow
             Console.WriteLine("[WARNING]: Server does not have a database connected. Clients that require a database won't be able to function properly.")
@@ -125,5 +174,15 @@ Module Program
             Console.WriteLine("[ERROR]: An error occurred while checking for updates. Please visit {0} for the latest version. Reason: {1}", githubUrl, ex.Message)
             Console.ForegroundColor = ConsoleColor.White
         End Try
+    End Sub
+
+    Function GenerateRandomCode() As Integer
+        Return New Random().Next(100000, 999999)
+    End Function
+
+    Sub ShowErrorMessageBox(crashReason As String)
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine("[ERROR]: The server has unexpectedly crashed. Reason: {0}", crashReason)
+        Console.ForegroundColor = ConsoleColor.White
     End Sub
 End Module
